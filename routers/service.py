c17cc6e7bd2db86b63cc7b778b645e1bac992f17
Kuldyaev <slavalion1@gmail.com>
2024-12-16 17:15:00 +0200

    Service router to docs

--- routers/service.py
+++ routers/service.py
@@ -1,10 +1,13 @@
-from aiogram import Router
+from fastapi import APIRouter
 from bot.create_bot import start_bot
 
 
-service_router = Router()
+service_router = APIRouter(
+   prefix="/service",
+   tags=["Service"],
+)
 
-@service_router.get('/')
+@service_router.get('/start')
 async def service_start() -> None:
     await start_bot()
     return "test"
