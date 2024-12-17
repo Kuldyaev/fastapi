615b0a6eb2eb568013d2b1faf0055185b703c8b1
Kuldyaev <slavalion1@gmail.com>
2024-12-18 01:16:04 +0200

    home page test

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from routers import router as api_router
 import uvicorn
 from config import config
 
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
 
@@ -18,25 +20,23 @@ app.add_middleware(
    allow_origins=["*"],
    allow_credentials=True,
    allow_methods=["*"],
-   allow_headers=["*"],
- 
+   allow_headers=["*"], 
 )
+
+templates = Jinja2Templates(directory='templates')
 
 app.include_router(api_router)
 
-
-
-@app.get("/test")
-async def test_route():
-   await start_bot()
-   return "test"
-
 @app.post("/webhook")
 async def webhook(request: Request) -> None:
-    update = Update.model_validate(await request.json(), context={"bot": bot})
-    await dp.feed_update(bot, update)
+   update = Update.model_validate(await request.json(), context={"bot": bot})
+   await dp.feed_update(bot, update)
+    
+@app.post("/")
+async def home_page():
+   return templates.TemplateResponse(name='home.html')
 
 application = ASGIMiddleware(app)
 
 if __name__ == "__main__":    
-    uvicorn.run(application, host='0.0.0.0', log_level='info')
+   uvicorn.run(application, host='0.0.0.0', log_level='info')
