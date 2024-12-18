b49bcc84ed3b4fc54f3afbba7f0cc01c05612988
Kuldyaev <slavalion1@gmail.com>
2024-12-18 15:48:55 +0200

    websocket create

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from a2wsgi import ASGIMiddleware
@@ -22,6 +22,13 @@ app.include_router(api_router)
 @app.get("/")
 async def home_page(request: Request):
    return templates.TemplateResponse(name='home.html', context={'request': request})
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
 
 application = ASGIMiddleware(app)
 
