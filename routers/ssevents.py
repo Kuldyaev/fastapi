029e06752b0c6520846fa29ee350f6d24adcdca2
Kuldyaev <slavalion1@gmail.com>
2025-01-02 14:18:17 +0200

    sse test 1

--- routers/ssevents.py
+++ routers/ssevents.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
+import time
 
 ssevents_router = APIRouter(
    prefix="/ssevents",
    tags=["SSEvents"],
 )
 
-async def fake_video_streamer():
+def fake_video_streamer():
     for i in range(10):
-        yield b"some fake video bytes"
+        yield f"frame {i}\n"
+        time.sleep(1)
 
 @ssevents_router.get("/")
 async def main():
-    return StreamingResponse(fake_video_streamer())
+    return StreamingResponse(fake_video_streamer(), media_type="text/plain")
