53dac5501d9b01086739a1671e04e88bb2b92ed0
Kuldyaev <slavalion1@gmail.com>
2025-01-03 23:16:12 +0200

    fix error for link

--- bot/keyboard.py
+++ bot/keyboard.py
@@ -6,10 +6,10 @@ from config import Config
 def app_keyboard(user_id: int, is_new_user: bool) -> InlineKeyboardMarkup:
     kb = InlineKeyboardBuilder()
     if is_new_user:
-         url_add_application = f"{Config.BASE_URL}/index.html?user_id={user_id}"
+         url_add_application = f"{Config.BASE_URL}index.html?user_id={user_id}"
          kb.button(text="Участвовать", callback_data="participate")
     else:
-         url_add_application = f"{Config.BASE_URL}/index.html?user_id={user_id}"
+         url_add_application = f"{Config.BASE_URL}index.html?user_id={user_id}"
          kb.button(text="Приложение", web_app=WebAppInfo(url=url_add_application))
         
     
