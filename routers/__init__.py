cce23b3f8f29ca9bae90d456634ddeff68752224
Kuldyaev <slavalion1@gmail.com>
2025-01-04 00:38:15 +0200

    pages router

--- routers/__init__.py
+++ routers/__init__.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter
 from .users import user_router
 from .service import service_router
 from .ssevents import ssevents_router
+from .pages import pages_router
 
 
 router = APIRouter()
 router.include_router(user_router)
 router.include_router(service_router)
 router.include_router(ssevents_router)
-
+router.include_router(pages_router)
 
